70cf577de9322dac414244f6381bf64183a123b8
Matias Carrasco Kind <mgckind@gmail.com>
2018-07-23 14:54:10 -0500

    rc0 1.0.0

--- keepcli/version.py
+++ keepcli/version.py
@@ -2,7 +2,7 @@
 
 commit = '49a3560'
 
-version_tag = (1, 0, 0, 'dev-{}'.format(commit))
+version_tag = (1, 0, 0, 'rc0'.format(commit))
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
